fcc3a6576cdb78caf2ffacde34d9e0171c43daeb
aogilvie <aogilvie@wizcorp.jp>
2014-11-20 11:38:06 +0900

    updated folder structure

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,14 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "phonegap/plugin/wizAssets/wizAssets.js",
+        "id": "jp.wizcorp.phonegap.plugin.wizAssetsPlugin",
+        "clobbers": [
+            "window.wizAssets"
+        ]
+    }];
+module.exports.metadata = 
+// TOP OF METADATA
+{}
+// BOTTOM OF METADATA
+});
--- www/phonegap/plugin/wizAssets/wizAssets.js
+++ www/phonegap/plugin/wizAssets/wizAssets.js
@@ -1,58 +1,31 @@
 /* Download and show, PhoneGap Example
  *
  * @author Ally Ogilvie
- * @copyright WizCorp Inc. [ Incorporated Wizards ] 2011
+ * @copyright Wizcorp Inc. [ Incorporated Wizards ] 2014
  * @file - wizAssets.js
  * @about - JavaScript download and update asset example for PhoneGap
  *
  *
  */
-
 var exec = require("cordova/exec");
-
 var wizAssets = { 
-    
-    
-	downloadFile: function(url, filePath, s, f) {
-	    
-	    window.setTimeout(function () {
-	                      cordova.exec(s, f, "WizAssetsPlugin", "downloadFile", [url, filePath]);
-	                      }, 0);
-	},
-	    
-	deleteFile: function(uri, s, f) {
-	    
-	    return cordova.exec(s, f, "WizAssetsPlugin", "deleteFile", [uri]);
-	    
-	},
-	    
-	    
-	deleteFiles: function(uris, s, f) {
-	    
-	    return cordova.exec(s, f, "WizAssetsPlugin", "deleteFiles", uris );
-	    
-	},
-	    
-	    
-	getFileURIs: function(s, f) {
-	    
-	    return cordova.exec(s, f, "WizAssetsPlugin", "getFileURIs", [] );
-	    
-	},
-	    
-	    
-	getFileURI: function(uri, s, f) {
-	    
-	    return cordova.exec(s, f, "WizAssetsPlugin", "getFileURI", [uri] );
-	    
-	},
-	    
-    purgeEmptyDirectories: function(s, f) {
-        // todo
-        s();
-        
+    downloadFile: function (url, filePath, s, f) {
+        window.setTimeout(
+            function () {
+                exec(s, f, "WizAssetsPlugin", "downloadFile", [url, filePath]);
+            }, 0);
+    },
+    deleteFile: function (uri, s, f) {
+        return exec(s, f, "WizAssetsPlugin", "deleteFile", [uri]);
+    },
+    deleteFiles: function (uris, s, f) {
+        return exec(s, f, "WizAssetsPlugin", "deleteFiles", uris );
+    },
+    getFileURIs: function (s, f) {
+        return exec(s, f, "WizAssetsPlugin", "getFileURIs", [] );
+    },
+    getFileURI: function (uri, s, f) {
+        return exec(s, f, "WizAssetsPlugin", "getFileURI", [uri] );
     }
-    
-    
 };
 module.exports = wizAssets;
